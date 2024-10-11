0d3c8f6e7ac1756aa8141b9b6be87778f3f8bb7f
DarkestIowaMedac <153643708+DarkestIowaMedac@users.noreply.github.com>
2024-10-11 20:10:00 +0200

    TareaTema5

---
+++ explicacion/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Tema5Controller;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/intento1', function () {
+    echo('hola');
+});
+
+Route::get('/intento2', function () {
+    $usuarios = User::all();
+    return view('welcome')->with(['users'=>$usuarios]);
+});
+
+Route::get('/intento3', [UserController::class, 'create']); //En la forma corta
+Route::get('/intento4', [UserController::class, 'prueba']); //En la forma corta
+Route::get('/esPrimo', [Tema5Controller::class, 'esPrimo']);
---
+++ saludo.js
@@ -0,0 +1,1 @@
+alert("Hola soy Rafa desde WAMPPIUUSSS")
